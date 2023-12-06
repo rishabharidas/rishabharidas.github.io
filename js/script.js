1b655f05280ffbdac74ad58d4cf0657183aa0ad9
rishabp <rishabp@alokin.in>
2023-12-06 18:36:50 +0530

    add navigator view options

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 let moreMenuOpened = false;
 
 function changeAndExpand() {
-  if (moreMenuOpened) {
-    document.getElementById("moreSection").style.height = "0";
+	if (moreMenuOpened) {
+		document.getElementById('moreSection').style.height = '0';
 
-    moreMenuOpened = !moreMenuOpened;
-  } else {
-    document.getElementById("moreSection").style.height = "160px";
-    moreMenuOpened = !moreMenuOpened;
-  }
+		moreMenuOpened = !moreMenuOpened;
+	} else {
+		document.getElementById('moreSection').style.height = '160px';
+		moreMenuOpened = !moreMenuOpened;
+	}
+}
+
+function showNavigator(value = true) {
+	if (value) {
+		document.getElementById('navigator').style.display = 'flex';
+		document.getElementById('drive-up-list').style.display = 'none';
+	} else {
+		document.getElementById('navigator').style.display = 'none';
+		document.getElementById('drive-up-list').style.display = 'flex';
+	}
 }
