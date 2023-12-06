cefeaa9483010d5314b7702e7ec4e198e02886d7
rishabp <rishabp@alokin.in>
2023-12-06 18:02:15 +0530

    add navigation bar to botton

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 let moreMenuOpened = false;
-setInterval(startComet, 35000);
-
-
-function startComet() {
-  let cometElement = document.getElementById("comet");
-  cometElement.style.animation = "comet-fall 5s linear";
-  setTimeout(() => {
-    cometElement.style.animation = "none";
-  }, 15000);
-}
 
 function changeAndExpand() {
   if (moreMenuOpened) {
