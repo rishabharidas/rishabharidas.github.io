40ab5eb4b13f2547ea347a3ee67b9ecb388347a0
rishabp-alokin <rishabp@alokin.in>
2024-04-21 00:36:37 +0530

    style update for navigator

--- js/script.js
+++ js/script.js
@@ -12,18 +12,18 @@ function showNavigator() {
 	const menuSvg =
 		'<path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />';
 	if (!navOpened) {
-		sideNav2.style.height = '160px';
-		sideNav2.style.width = '160px';
+		sideNav2.style.height = '140px';
+		sideNav2.style.width = '140px';
 		navOpened = true;
 		svgIcon.style.width = '45px';
-		svgIcon.style.right = '9%';
+		svgIcon.style.right = '6%';
 		svgIcon.style.bottom = '6%';
 		menuItem1.style.display = 'flex';
 
 		svgIcon.innerHTML = closeSvg;
 	} else {
-		sideNav2.style.height = '90px';
-		sideNav2.style.width = '90px';
+		sideNav2.style.height = '80px';
+		sideNav2.style.width = '80px';
 		svgIcon.style.width = '35px';
 		svgIcon.style.right = '18%';
 		svgIcon.style.bottom = '12%';
