8f959fc3fdc9639b2c8f7c11dd5691cea34471dc
Rishab Haridas <rishabharidas010@gmail.com>
2025-04-10 16:25:45 +0530

    update sw to root

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function isNavOpened() {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('js/sw.js', { scope: './' })
+  navigator.serviceWorker.register('sw.js')
   .then(reg => console.log('Service Worker registered with scope:', reg.scope))
   .catch(err => console.error('Service Worker registration failed:', err));
 }
